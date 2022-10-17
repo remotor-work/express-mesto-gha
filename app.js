1f1793e7dc3513df90cad1bd342514f63cd2d265
Igor Savin <remotor-work@yandex.ru>
2022-10-17 18:50:11 -0400

    13.1

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const mongoose = require("mongoose");
 
 const { PORT = 3000, MONGO_URL = "mongodb://localhost:27017/mestodb" } =
   process.env;
+
 const app = express();
 
 app.use(express.json()); //insted body parser (req.body) ??????? do i need?!! // Without `express.json()`, `req.body` is undefined.
@@ -15,6 +16,11 @@ mongoose.connect(MONGO_URL, {
 });
 
 // Здесь роутинг
+app.get("/", (req, res) => {
+  res.status(404);
+  res.send("<h1>Страница не найдена</h1>");
+});
+
 app.use("/users", (req, res, next) => {
   req.user = {
     _id: "6349e25ea0300e5d282d2201", // вставьте сюда _id созданного в предыдущем пункте пользователя
@@ -34,17 +40,6 @@ app.use("/cards", (req, res, next) => {
 });
 
 app.use("/cards", require("./routes/cards"));
-
-//!!!!!!!!! В файле app.js создайте вот такой мидлвэр:
-//Это временное решение. Мы захардкодили идентификатор пользователя, поэтому кто бы ни создал карточку, в базе у неё будет один и тот же автор. Ничего страшного — исправим это в следующем спринте.
-// app.use((req, res, next) => {
-//   req.user = {
-//     _id: "6349e25ea0300e5d282d2201", // вставьте сюда _id созданного в предыдущем пункте пользователя
-//   };
-
-//   next();
-// });
-//!!!!!!!!!
 
 module.exports.createCard = (req, res) => {
   console.log(req.user._id); // _id станет доступен
