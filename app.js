737a8940f4044baedaa794469ac592c8a9b31489
Igor Savin <remotor-work@yandex.ru>
2022-10-19 13:49:00 -0400

    13.2

--- app.js
+++ app.js
@@ -23,6 +23,7 @@ app.use((req, res, next) => {
 
 app.use('/users', users);
 app.use('/cards', cards);
+app.use('*', (req, res) => res.status(404).send({ message: 'Страница не найдена' }));
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
