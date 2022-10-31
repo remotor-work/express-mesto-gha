1432ef27215326801ec1d3dec406f09176382b1a
Igor Savin <remotor-work@yandex.ru>
2022-10-31 16:31:15 -0400

    14.1

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
-const helmet = require('helmet');
 const rateLimit = require('express-rate-limit');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
@@ -16,7 +15,6 @@ const app = express();
 const limiter = rateLimit({ windowMs: 15 * 60 * 1000, max: 100 });
 
 app.use(express.json());
-app.use(helmet());
 app.use(limiter);
 app.use(cookieParser());
 app.use(routes);
